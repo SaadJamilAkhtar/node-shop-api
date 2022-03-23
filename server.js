9b70f85ccd4c85d61196fca472de135942956378
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-03-23 16:58:51 +0500

    added logging and error handler

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
+const colors = require('colors')
+const morgan = require('morgan')
 const port = process.env.PORT || 5000;
 const productRoutes = require('./api/routes/productRoutes');
-const ordersRoutes = require('./api/routes/orderRoutes')
+const ordersRoutes = require('./api/routes/orderRoutes');
+const errorHandler = require('./api/middleware/errorHandler');
 
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
 
+app.use(errorHandler);
 
-
-
-
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Server Listening at port : ${port} ...`)
 });
